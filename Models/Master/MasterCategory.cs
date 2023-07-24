f4f9b86c5b518684ef5b108e3809684a3db28f34
handoyo1992 <129417491+handoyo1992@users.noreply.github.com>
2023-07-24 17:08:56 +0700

    Add Category
    Tambah model Category

---
+++ Models/Master/MasterCategory.cs
@@ -0,0 +1,64 @@
+﻿using Datalist;
+using eShop.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace eShop.Models
+{
+    public class MasterCategory
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [DatalistColumn]
+        [Required(ErrorMessage = "Kode Kategori harus diisi.")]
+        [Index("IX_Code", Order = 1, IsUnique = true)]
+        [Display(Name = "Kode Kategori")]
+        [StringLength(128, ErrorMessage = "Maksimal 128 huruf.")]
+        [Remote("IsCodeExists", "MasterKategoris", AdditionalFields = "Id", ErrorMessage = "This code has been used.")]
+        public string Code { get; set; }
+
+        [DatalistColumn]
+        [Required(ErrorMessage = "Nama Kategori harus diisi.")]
+        [Display(Name = "Nama Kategori")]
+        [StringLength(256, ErrorMessage = "Maksimal 128 huruf.")]
+        public string Name { get; set; }
+
+        [DatalistColumn]
+        [Display(Name = "Keterangan")]
+        [DataType(DataType.MultilineText)]
+        public string Notes { get; set; }
+
+        [Display(Name = "Default")]
+        public bool Default { get; set; }
+
+        [Display(Name = "Aktif")]
+        public bool Active { get; set; }
+
+        [Display(Name = "Dibuat")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss tt}", ApplyFormatInEditMode = true)]
+        public DateTime Created { get; set; }
+
+        [Display(Name = "Diubah")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss tt}", ApplyFormatInEditMode = true)]
+        public DateTime Updated { get; set; }
+
+        [Display(Name = "User")]
+        public int UserId { get; set; }
+
+        [Display(Name = "User")]
+        public virtual ApplicationUser User { get; set; }
+    }
+
+   
+}
