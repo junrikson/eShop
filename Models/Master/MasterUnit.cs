35d6b8e71fddda26b8fe7b199693af9edcba04d5
handoyo1992 <129417491+handoyo1992@users.noreply.github.com>
2023-07-24 16:34:02 +0700

    Add Unit
    Tambah modul Satuan

---
+++ Models/Master/MasterUnit.cs
@@ -0,0 +1,61 @@
+﻿using Datalist;
+using eShop.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace eShop.Models
+{
+    public class MasterUnit
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [DatalistColumn]
+        [Required(ErrorMessage = "Item unit must be filled.")]
+        [Index("IX_Code", Order = 1, IsUnique = true)]
+        [Display(Name = "Item Unit")]
+        [StringLength(128, ErrorMessage = "Maximum 128 Characters.")]
+        [Remote("IsCodeExists", "MasterUnit", AdditionalFields = "Id", ErrorMessage = "This code has been used.")]
+        public string Code { get; set; }
+
+        [DatalistColumn]
+        [Required(ErrorMessage = "Unit name must be filled.")]
+        [Display(Name = "Item Name")]
+        [StringLength(256, ErrorMessage = "Maximum 128 Characters.")]
+        public string Name { get; set; }
+
+        [DatalistColumn]
+        [Display(Name = "Keterangan")]
+        [DataType(DataType.MultilineText)]
+        public string Notes { get; set; }
+
+        [Display(Name = "Aktif")]
+        public bool Active { get; set; }
+
+        [Display(Name = "Dibuat")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss tt}", ApplyFormatInEditMode = true)]
+        public DateTime Created { get; set; }
+
+        [Display(Name = "Diubah")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss tt}", ApplyFormatInEditMode = true)]
+        public DateTime Updated { get; set; }
+
+        [Display(Name = "User")]
+        public int UserId { get; set; }
+
+        [Display(Name = "User")]
+        public virtual ApplicationUser User { get; set; }
+    }
+
+   
+}
