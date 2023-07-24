6e517cf55f8240e09848b0a8fc0d71b469d57ad7
handoyo1992 <129417491+handoyo1992@users.noreply.github.com>
2023-07-24 20:46:44 +0700

    Update master Unit
    create,del,save

--- Models/Settings/IdentityModels.cs
+++ Models/Settings/IdentityModels.cs
@@ -129,7 +129,7 @@ namespace eShop.Models
         public System.Data.Entity.DbSet<eShop.Models.MasterBusinessUnit> MasterBusinessUnits { get; set; }
         public System.Data.Entity.DbSet<eShop.Models.MasterRegion> MasterRegions { get; set; }
         public System.Data.Entity.DbSet<eShop.Models.MasterItem> MasterItems { get; set; }
-        public System.Data.Entity.DbSet<eShop.Models.MasterItemUnits> MasterItemsUnits { get; set; }
+        //public System.Data.Entity.DbSet<eShop.Models.MasterItemUnits> MasterItemsUnits { get; set; }
         public System.Data.Entity.DbSet<eShop.Models.MasterUnit> MasterUnits { get; set; }
         public System.Data.Entity.DbSet<eShop.Models.MasterRegionAccount> MasterRegionAccounts { get; set; }
         public System.Data.Entity.DbSet<eShop.Models.MasterCustomer> MasterCustomers { get; set; }
